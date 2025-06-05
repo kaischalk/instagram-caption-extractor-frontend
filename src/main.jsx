21b23d80527f4c70a039eedb174ce9458c1391ee
kaischalk <kaischalk@googlemail.com>
2025-06-05 21:09:59 +0200

    New from chat

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ExtractPage from './pages/ExtractPage';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ExtractPage cookies={{}} />
+  </React.StrictMode>
+);
